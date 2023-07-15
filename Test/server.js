dff3a467d150393d12cdd7667edc473561487616
kousakakeito <139373779+kousakakeito@users.noreply.github.com>
2023-07-16 02:42:16 +0900

    各Formの連携

--- Test/server.js
+++ Test/server.js
@@ -5,6 +5,18 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/register.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Public/Form/register/register.html'));
+});
+
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Public/index.html'));
+});
+
+app.get('/reset.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Public/Form/reset/reset.html'));
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
