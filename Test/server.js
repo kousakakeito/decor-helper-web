eba3883d30bc04b495a29212ee68daa64c7d08c1
kousakakeito <139373779+kousakakeito@users.noreply.github.com>
2023-07-16 00:45:12 +0900

    Expressでローカルwebサーバー、ディレクトリ構成変更

--- Test/server.js
+++ Test/server.js
@@ -1,43 +1,14 @@
-const http = require('http'); 
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((request, response) => {
-  const indexPath = path.join(__dirname, 'register', 'index.html');
-  const cssPath = path.join(__dirname, 'register', 'stylesheet.css');
-  const jsPath = path.join(__dirname, 'register', 'index.js');
-  
-  fs.readFile(indexPath, 'utf8', (error, htmlData) => {
-    if (error) {
-      response.writeHead(404, { 'Content-Type': 'text/plain' });
-      response.write('File not found');
-      response.end();
-    } else {
-      fs.readFile(cssPath, 'utf8', (error, cssData) => {
-        if (error) {
-          response.writeHead(500, { 'Content-Type': 'text/plain' });
-          response.write('Internal Server Error');
-          response.end();
-        } else {
-          fs.readFile(jsPath, 'utf8', (error, jsData) => {
-            if (error) {
-              response.writeHead(500, { 'Content-Type': 'text/plain' });
-              response.write('Internal Server Error');
-              response.end();
-            } else {
-              const fullHtml = htmlData.replace('</head>', `<style>${cssData}</style></head>`)
-                                       .replace('</body>', `<script>${jsData}</script></body>`);
-              response.writeHead(200, { 'Content-Type': 'text/html' });
-              response.write(fullHtml);
-              response.end();
-            }
-          });
-        }
-      });
-    }
-  });
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-server.listen(8080, () => {
-  console.log('Server is running on port 8080');
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
