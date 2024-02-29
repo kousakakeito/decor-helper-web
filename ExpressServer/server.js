a10644345131001d3570df558a1f915add3f39fe
kousakakeito <139373779+kousakakeito@users.noreply.github.com>
2024-03-01 02:48:51 +0900

    本番環境移行設定更新

--- ExpressServer/server.js
+++ ExpressServer/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cookieParser = require('cookie-parser'); // cookie-parserモジュールを追加
 
 // 静的ファイルの設定
